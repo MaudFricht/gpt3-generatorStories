83566d01f826dabf327a93831f0275031261f340
MaudFri <maud93frichement@gmail.com>
2022-12-23 21:41:06 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -19,23 +19,6 @@ export default function Document() {
         <meta name="author" content="Maud Frichement" />
 
 
-<meta name="description" content="iyagi - Générateur d'histoires pour enfants"/>
-
-<meta itemprop="name" content="iyagi"/>
-<meta itemprop="description" content="iyagi - Générateur d'histoires pour enfants"/>
-<meta itemprop="image" content="https://zupimages.net/viewer.php?id=22/51/teag.png"/>
-
-<meta property="og:url" content="https://www.iyagi.xyz"/>
-<meta property="og:type" content="website"/>
-<meta property="og:title" content="iyagi"/>
-<meta property="og:description" content="iyagi - Générateur d'histoires pour enfants"/>
-<meta property="og:image" content="https://zupimages.net/viewer.php?id=22/51/teag.png"/>
-
-<meta name="twitter:card" content="summary_large_image"/>
-<meta name="twitter:title" content="iyagi"/>
-<meta name="twitter:description" content="iyagi - Générateur d'histoires pour enfants"/>
-<meta name="twitter:image" content="https://zupimages.net/viewer.php?id=22/51/teag.png"/>
-
       </Head>
       <body>
         <Main />
